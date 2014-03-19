83892bb11fc23a3518ba40574c85d46123487713
johnjarrell <jarrelljohn@msn.com>
2014-03-19 11:17:06 -0500

    Homework1
    PFAB Homework1 03/17/2014

---
+++ HomeWork.py
@@ -0,0 +1,32 @@
+#!user/bin/python
+import math
+ 
+#First Name
+first = "John "
+ 
+#Last Name
+last = "Jarrell "
+ 
+#My age
+age = "42"
+ 
+print first + last + age
+ 
+sideA = 12.55
+sideB = 17.85
+ 
+sideC = (sideA**2) + (sideB**2)
+ 
+print math.sqrt(sideC)
+ 
+#Define numbers for calculation
+operand1 = 95
+operand2 = 64.5
+ 
+ 
+#print and do calculation on numbers
+print operand1, "+", operand2, "=", operand1 + operand2
+print operand1, "-", operand2, "=", operand1 - operand2
+print operand1, "*", operand2, "=", operand1 * operand2
+print operand1, "/", operand2, "=", operand1 / operand2
+print operand1, "%", operand2, "=", operand1 % operand2
